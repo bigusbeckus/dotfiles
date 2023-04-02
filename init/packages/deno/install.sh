10714ea072d2b6d7a4d99349f6df413b323b1eda
Beck <b.tadesse.77@gmail.com>
2023-04-02 14:05:26 +0300

    Skip deno install if it exists

--- init/packages/deno/install.sh
+++ init/packages/deno/install.sh
@@ -2,18 +2,22 @@
 set -euo pipefail
 
 function install_deno() {
-	# Install deno and add it to PATH
-	curl -fsSL https://deno.land/x/install/install.sh | sh
-	# zsh
-	echo "" >>~/.zshrc
-	echo "# Deno" >>~/.zshrc
-	echo "export DENO_INSTALL=$HOME/.deno" >>~/.zshrc
-	sed -Ei 's/(export PATH=.+)\:\$PATH/\1\:\$HOME\/\.deno\/bin\:\$PATH/g' ~/.zshrc
-	# bash
-	echo "" >>~/.bashrc
-	echo "# Deno" >>~/.bashrc
-	echo "export DENO_INSTALL=$HOME/.deno" >>~/.bashrc
-	echo "export PATH=\$HOME/.deno/bin:\$PATH" >>~/.bashrc
+	if ! command -v deno &>/dev/null; then
+		# Install deno and add it to PATH
+		curl -fsSL https://deno.land/x/install/install.sh | sh
+		# zsh
+		echo "" >>~/.zshrc
+		echo "# Deno" >>~/.zshrc
+		echo "export DENO_INSTALL=$HOME/.deno" >>~/.zshrc
+		sed -Ei 's/(export PATH=.+)\:\$PATH/\1\:\$HOME\/\.deno\/bin\:\$PATH/g' ~/.zshrc
+		# bash
+		echo "" >>~/.bashrc
+		echo "# Deno" >>~/.bashrc
+		echo "export DENO_INSTALL=$HOME/.deno" >>~/.bashrc
+		echo "export PATH=\$HOME/.deno/bin:\$PATH" >>~/.bashrc
+    
+    source ~/.bashrc
+	fi
 }
 
 # Install
