ae05ebf0d579bc4a85864f69de7d9172d326ce51
Beck <b.tadesse.77@gmail.com>
2023-04-04 00:10:46 +0300

    Add tmux config

--- config/pull.sh
+++ config/pull.sh
@@ -4,15 +4,26 @@
 nvim_config_dir=~/.config/nvim
 zsh_config_dir=~/.oh-my-zsh
 git_config_filename=.gitconfig
+tmux_config_filename=.tmux.conf
 git_config_file=~/$git_config_filename
+tmux_config_file=~/$tmux_config_filename
 
-# Copy over .gitconfig if exists
+# Copy over .gitconfig if it exists
 if [ -f "$git_config_file" ]; then
 	# Create the git directory if it doesn't already exist
 	mkdir -p git
 	# Copy the config file
 	cp "$git_config_file" git/"$git_config_filename"
 	echo "Git global config copied."
+fi
+
+# Copy over .tmux.conf if it exists
+if [ -f "$tmux_config_file" ]; then
+	# Create the tmux directory if it doesn't already exist
+	mkdir -p tmux
+	# Copy the config file
+	cp "$tmux_config_file" tmux/"$tmux_config_filename"
+	echo "Tmux config file copied."
 fi
 
 # Check if the config directories exist
