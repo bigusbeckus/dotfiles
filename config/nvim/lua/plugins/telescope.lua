585a893284458f736df27d6982badb10c9bb7f20
unknown <b.tadesse.77@gmail.com>
2024-12-31 04:20:23 +0300

    Fix telescope config on fresh installations

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -4,35 +4,36 @@ return {
 		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
-
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
 		build = "make",
-		opts = {
-			defaults = {
-				file_ignore_patterns = {
-					"node%_modules/.*",
-					"%.git/.*",
-					"^dist/.*",
-					"^.expo/.*",
-					"^.next/.*",
-					"^.nuxt/.*",
-					"^.terraform/.*",
-					"^.venv/.*",
-					"^.venv...+/.*",
-				},
-				mappings = {
-					i = {
-						["<Esc>"] = require("telescope.actions").close,
+		config = function()
+			-- Has to be in the config function vs a static table in opts
+			-- because we need telescope loaded in before the
+			-- `require("telescope.actions").close` line is resolved
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						"node%_modules/.*",
+						"%.git/.*",
+						"^dist/.*",
+						"^.expo/.*",
+						"^.next/.*",
+						"^.nuxt/.*",
+						"^.terraform/.*",
+						"^.venv/.*",
+						"^.venv...+/.*",
+					},
+					mappings = {
+						i = {
+							["<Esc>"] = require("telescope.actions").close,
+						},
 					},
 				},
-			},
-			pickers = {
-				layout_strategy = "vertical",
-			},
-		},
-		config = function(_, opts)
-			require("telescope").setup(opts)
+				pickers = {
+					layout_strategy = "vertical",
+				},
+			})
 			require("telescope").load_extension("fzf")
 
 			-- Configure telescope (not in `after` because sometimes there's an annoying delay before the keybinds kick in)
